6cde6392f84a3cb9ebf9d6fa28e8fdb5bf8980e4
swathireddappa <swathireddy1133@gmail.com>
2023-05-31 15:02:00 +0200

    created the images in block using flex

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
+import  ProductCard from "./ProductCard";
+import styles from "./ProductList.module.css"
 
-
- export const ProductList = () =>{
+const ProductList = () =>{
     const [ products, setProducts ] = useState([]);
-
+    const [ selectedCategory, setSelectedCategory] = useState([]);
+    const categories = [ ...new Set(products.map((product) => product.category))];
 
 useEffect (()  => {
     fetch('https://dummyjson.com/products')
-    .then((response) => response.json())
+    .then((response)=> response.json())
     .then((data) => {
         setProducts(data.products);
         console.log(data.products);
@@ -15,15 +17,42 @@ useEffect (()  => {
     .catch((error) => {
         console.error('Error fetching products :', error);
 
-    });
+    })      
 }, []);
 
+const handleCategorySelect = (event) => {
+    setSelectedCategory(event.target.value);
+}
 return(
-    <div>
-        <h1>Product List</h1>
+    <div className={styles.ListWrapper}>
+        <div className={styles.controlsWrapper}>
+        <label htmlFor='category'>Filter by category:</label>
+        <select
+          id='category'
+          className={styles.inputField}
+           value={selectedCategory}
+          onChange={handleCategorySelect}
+        >
+          <option value=''>All</option>
+          { categories.map((category, index) => (
+            <option key={index} value={category}>
+              {category}
+            </option>
+           ))
+          }
+        </select>
+      </div>
+
+          <div className={styles.cardsWrapper }>
+        { products 
+        .filter((product) => selectedCategory ==  '' || product.category === selectedCategory)
+        .map((product) => 
+            <ProductCard key ={ product.id} product = {product} />
+         )}
+         </div>
     </div>
 
 );
-}  
+    }
 
-
+export default ProductList
